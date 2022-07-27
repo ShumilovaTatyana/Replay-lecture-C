4e38bed6c144115530abd69578de20756d5c0ae5
Shumilova Tatyana <star28@inbox.ru>
2022-07-27 19:59:38 +0300

    Add Lecture2

---
+++ Lecture2/003/Program.cs
@@ -0,0 +1,16 @@
+﻿// Есть массив array из n элементов, требуется найти элемент массива, равный find
+
+int[] array = { 11, 222, 33, 40, 75, 96, 67, 11 };
+int n = array.Length;
+int find = 11;
+
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index = index + 1;
+}
